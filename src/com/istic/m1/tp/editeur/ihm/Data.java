68b2ea6bd43ccc73196402813fa407019d4ba989
SmartDevCI <info@appsmartdev.com>
2017-10-13 18:05:40 +0200

    interface vide

---
+++ src/com/istic/m1/tp/editeur/ihm/Data.java
@@ -0,0 +1,10 @@
+package com.istic.m1.tp.editeur.ihm;
+
+public class Data {
+
+	public static String EditorName="Mini Editeur";
+	public static int height=600;
+	public static int width=800;
+	
+
+}
--- src/com/istic/m1/tp/editeur/ihm/MonIHM.java
+++ src/com/istic/m1/tp/editeur/ihm/MonIHM.java
@@ -1,15 +1,105 @@
 package com.istic.m1.tp.editeur.ihm;
 
+import java.awt.BorderLayout;
+
 import javax.swing.*;
 
 public class MonIHM extends JFrame{
+
+	//La barre de menu
+	private JMenuBar barre_de_menu;
+
+	//Les menus
+	private JMenu menuFichier;
+	private JMenu menuEdition;
+
+	//Les JPanel pour gerer la mis en page de l'interface
+	private JPanel panneauPrincipal;
+	
+
+	//Les JMenuItem pour les bouton
+	private JMenuItem menuCopier;
+	private JMenuItem menuCouper;
+	private JMenuItem menuColler;
+	private JMenuItem menuFermer;
+	private JMenuItem menuEnregistrer;
+	
+	/***********FIN MENU**************/
+	
+	//Les composants de la fenêtres
+	private JTextArea zoneDeSaisie;
+	private JTextArea zoneDeNotification;
+	
+	
+	
+	/*
+	private JMenuItem menuCopier;*/
+
+
+
 
 
 
 	public MonIHM()
 	{
-		this.setSize(600, 600);  //definit la taille de la fenetre
+
+		this.setSize(Data.width, Data.height);  //definit la taille de la fenetre
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);  //Pour fermer la fenetre quand on clique sur la croix rouge (bouton fermer) de la fenetre
+		this.setTitle(Data.EditorName);  //defini le titre de l'editeur
+		this.setLocationRelativeTo(null);
+
+		//initialisation des composants de l'interface
+		this.initialize();
+
+		//placement des composants sur le menu Fichier
+		menuFichier.add(menuEnregistrer);
+		menuFichier.add(menuFermer);
+
+		//placement des composants sur le menu Edition
+		menuEdition.add(menuCopier);
+		menuEdition.add(menuCouper);
+		menuEdition.add(menuColler);
+		
+		//placement du Menu
+		barre_de_menu.add(menuFichier);
+		barre_de_menu.add(menuEdition);
+		this.setJMenuBar(barre_de_menu);
+				
+		
+		//Ajout au panneau
+		panneauPrincipal.setLayout(new BorderLayout());
+		zoneDeSaisie.setSize(Data.width, Data.height);
+		
+		panneauPrincipal.add(zoneDeSaisie,BorderLayout.CENTER);
+		this.setContentPane(panneauPrincipal);
+		
+		
+		
+		//Le corps
+
+
+
+
+	}
+
+
+
+
+	public void initialize()
+	{
+		barre_de_menu=new JMenuBar();
+		menuFichier=new JMenu("Fichier");
+		menuEdition=new JMenu("Edition");
+		panneauPrincipal=new JPanel();
+
+		menuCopier=new JMenuItem("Copier");
+		menuCouper=new JMenuItem("Couper");
+		menuColler=new JMenuItem("Coller");
+		menuFermer=new JMenuItem("Fermer");
+		menuEnregistrer=new JMenuItem("Enregistrer");
+		
+		zoneDeSaisie = new JTextArea();
+		zoneDeNotification=new JTextArea();
 	}
 
 
